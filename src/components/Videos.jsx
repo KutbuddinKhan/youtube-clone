a09cd251d497fa65d47cafd074d74fd6b113362e
Kutbuddin khan <Kutbuddinkhan82@gmail.com>
2023-09-30 01:58:51 +0530

    first commit

---
+++ src/components/Videos.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Stack, Box } from "@mui/material";
+
+import { ChannelCard, VideoCard } from "./";
+
+const Videos = ({ videos, direction }) => {
+  // Check if 'videos' is defined and is an array
+  if (!Array.isArray(videos) || videos.length === 0) {
+    return null; // You can return something else, like a message or loading indicator
+  }
+  
+  return (
+    <Stack direction={direction || "row"} flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
+      {videos.map((item, idx) => (
+        <Box key={idx}>
+          {item.id.videoId && <VideoCard video={item} /> }
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
+        </Box>
+      ))}
+    </Stack>
+  );
+}
+
+export default Videos;
